42f8611b2410ade0d956a6fb4867134bde602392
Duc-Thomas <duckiduc.dev@gmail.com>
2021-06-22 19:45:13 +0200

    Update vector images

--- app/components/help/card.tsx
+++ app/components/help/card.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 import styles from '../../styles/Help.module.css'
-import illustration from '../../../public/undraw_Hello.svg'
 
 export default function Card(props) {
     return (
         <div className={styles.card}>
             <p>{props.name}</p>
             <div className={styles.illustationContainer}>
-                <Image src={illustration} alt="Picture of the author" />
+                <Image src={props.image} layout="fill" objectFit="contain" alt="Picture of the author" />
             </div>
         </div>
     )
--- pages/help.tsx
+++ pages/help.tsx
@@ -1,6 +1,12 @@
 import Card from '../app/components/help/card'
 import Header from '../app/components/header'
 import styles from '../app/styles/Help.module.css'
+import time from '../public/time.svg'
+import newMessage from '../public/newMessage.svg'
+import questions from '../public/questions.svg'
+import security from '../public/security.svg'
+import warning from '../public/warning.svg'
+import faq from '../public/faq.svg'
 
 const cardsTitle = {
     outOfDate: "Mon ordonnance est périmée, que faire ?",
@@ -16,12 +22,12 @@ export default function Help() {
         <div className={styles.background}>
             <Header name={"Besoin d'aide ?"} />
             <div className={styles.container}>
-                <Card name={cardsTitle.outOfDate} />
-                <Card name={cardsTitle.renewal} />
-                <Card name={cardsTitle.treatement} />
-                <Card name={cardsTitle.privacy} />
-                <Card name={cardsTitle.reportProblem} />
-                <Card name={cardsTitle.faq} />
+                <Card name={cardsTitle.outOfDate} image={time} />
+                <Card name={cardsTitle.renewal} image={newMessage} />
+                <Card name={cardsTitle.treatement} image={questions} />
+                <Card name={cardsTitle.privacy} image={security} />
+                <Card name={cardsTitle.reportProblem} image={warning} />
+                <Card name={cardsTitle.faq} image={faq} />
             </div>
         </div>
     )
