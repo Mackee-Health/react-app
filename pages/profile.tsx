67b321dcd60b914b64ef12d44826c86969149333
Duc-Thomas <duckiduc.dev@gmail.com>
2021-07-03 12:13:00 +0200

    Adapt for iPhone

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import Header from '../app/components/header'
-import styles from '../app/styles/Home.module.css'
+import styles from '../app/styles/Profile.module.css'
 import ProfileCard from '../app/components/profileCard';
 import heart from '../public/heart.svg'
 import phone from '../public/phone.svg'
@@ -12,31 +12,31 @@ export default function Home() {
             <Header name={"Mon profil"} />
             <main className={styles.main}>
                 <ProfileCard name={"Ornella"} />
-                <section className={styles.prescriptionsList}>
+                <section className={styles.informations}>
                     <p className={styles.headerText}>Mes informations</p>
                     <div className={styles.card}>
-                        <table className={styles.prescriptionsTable}>
+                        <table className={styles.informationsTable}>
                             <tr>
                                 <td>N° de sécurité sociale</td>
-                                <td className={styles.date}>1 23 456 789 ABCD</td>
+                                <td className={styles.detail}>1 23 456 789 ABCD</td>
                             </tr>
                             <tr>
                                 <td>Adresse</td>
-                                <td className={styles.date}>1 rue de Rivoli Paris 75000</td>
+                                <td className={styles.detail}>1 rue de Rivoli Paris 75000</td>
                             </tr>
                             <tr>
                                 <td>N° de téléphone</td>
-                                <td className={styles.date}>06 00 11 22 33</td>
+                                <td className={styles.detail}>06 00 11 22 33</td>
                             </tr>
                             <tr>
                                 <td>Email</td>
-                                <td className={styles.date}>ornella@gmail.com</td>
+                                <td className={styles.detail}>ornella@gmail.com</td>
                             </tr>
                         </table>
                     </div>
                 </section>
 
-                <section className={styles.pharmacies}>
+                <section className={styles.favorite}>
                     <p className={styles.headerText}>Mon médecin traitant</p>
                     <div className={styles.card}>
                         <div className={styles.header}>
@@ -55,7 +55,7 @@ export default function Home() {
                     </div>
                 </section>
 
-                <section className={styles.pharmacies}>
+                <section className={styles.favorite}>
                     <p className={styles.headerText}>Ma pharmacie préférée</p>
                     <div className={styles.card}>
                         <div className={styles.header}>
